15d341d890f867d479ca9667890b224ec5c68050
Peter Vegh <pvegh@posteo.uk>
2022-05-05 16:11:16 +0100

    Autodetect license

--- setup.py
+++ setup.py
@@ -1,16 +1,19 @@
 import ez_setup
+
 ez_setup.use_setuptools()
 
 from setuptools import setup, find_packages
 
-exec(open('proglog/version.py').read()) # loads __version__
+exec(open("proglog/version.py").read())  # loads __version__
 
-setup(name='proglog',
-      version=__version__,
-      author='Zulko',
-    description='Log and progress bar manager for console, notebooks, web...',
-    long_description=open('pypi-readme.rst').read(),
-    license='MIT - copyright Edinburgh Genome Foundry',
+setup(
+    name="proglog",
+    version=__version__,
+    author="Zulko",
+    description="Log and progress bar manager for console, notebooks, web...",
+    long_description=open("pypi-readme.rst").read(),
+    license="MIT",
     keywords="logger log progress bar",
-    install_requires=['tqdm'],
-    packages= find_packages(exclude='docs'))
+    install_requires=["tqdm"],
+    packages=find_packages(exclude="docs"),
+)
